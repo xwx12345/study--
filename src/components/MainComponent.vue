b318a597aa8f2c9ef7d82a9e056c89b3b5eacb88
IoNaNO <orange0714@foxmail.com>
2022-08-17 20:56:59 +0800

    Change Background

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -11,10 +11,17 @@
         mode="horizontal"
         background-color="#fdf6f9"
         text-color="#7E789A"
-        active-text-color="#DCA55F">
-        <el-menu-item index="/"><i class="el-icon-s-home"></i>首页</el-menu-item>
-        <el-menu-item index="/books"><i class="el-icon-reading"></i>书籍</el-menu-item>
-        <el-menu-item index="/courses"><i class="el-icon-s-cooperation"></i>课程</el-menu-item>
+        active-text-color="#DCA55F"
+      >
+        <el-menu-item index="/"
+          ><i class="el-icon-s-home"></i>首页</el-menu-item
+        >
+        <el-menu-item index="/books"
+          ><i class="el-icon-reading"></i>书籍</el-menu-item
+        >
+        <el-menu-item index="/courses"
+          ><i class="el-icon-s-cooperation"></i>课程</el-menu-item
+        >
       </el-menu>
     </div>
     <div class="main">
@@ -24,33 +31,33 @@
 </template>
 
 <script>
-import Header from './header.vue'
+import Header from "./header.vue";
 export default {
   components: {
-    Header
+    Header,
   },
-  data () {
+  data() {
     return {
-       activeIndex: '/'
-    }
+      activeIndex: "/",
+    };
   },
-  methods: {
-
-  },
-  mounted () {
-  },
-  created () {
-
-  }
-}
+  methods: {},
+  mounted() {},
+  created() {},
+};
 </script>
 
 <style scoped lang="scss">
-.container{
+.container {
   width: calc(100vw - 80px);
   margin: 0 auto;
-  .main{
-    background: linear-gradient(rgba(222,223,251,1) 0%,rgba(255,255,255,0) 99.794%),linear-gradient(rgba(255,216,211,1) 0%,rgba(255,194,220,0.7647058823529411) 100%);;
+  .main {
+    background: rgb(222, 223, 251);
+    background: linear-gradient(
+      40deg,
+      rgba(222, 223, 251, 1) 0%,
+      rgba(255, 194, 220, 1) 100%
+    );
   }
 }
 </style>
