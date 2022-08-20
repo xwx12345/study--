f7b8a635cf578fb05a511f682d57cd4ab0045b32
xwx12345 <1812299239@qq.com>
2022-08-20 20:39:18 +0800

    初步写好专家模块，等待具体数据

--- src/views/subject/answer.vue
+++ src/views/subject/answer.vue
@@ -9,6 +9,10 @@
           max-height="400">
           <el-table-column
             fixed
+            label="序号"
+            type="index">
+          </el-table-column>
+          <el-table-column
             prop="date"
             label="日期"
             width="150">
@@ -44,10 +48,10 @@
             width="120">
             <template slot-scope="scope">
               <el-button
-                @click.native.prevent="deleteRow(scope.$index, tableData)"
-                type="text"
-                size="small">
-                跳转
+                @click="Jump(scope.$index, tableData1)"
+                icon="el-icon-search"
+                circle
+                size="big">
               </el-button>
             </template>
           </el-table-column>
@@ -62,9 +66,8 @@
           max-height="400">
           <el-table-column
             fixed
-            prop="date"
-            label="日期"
-            width="150">
+            label="序号"
+            type="index">
           </el-table-column>
           <el-table-column
             prop="name"
@@ -93,15 +96,10 @@
           </el-table-column>
           <el-table-column
             fixed="right"
-            label="操作"
-            width="120">
+            label="题目编号"
+            width="180">
             <template slot-scope="scope">
-              <el-button
-                @click.native.prevent="deleteRow(scope.$index, tableData)"
-                type="text"
-                size="small">
-                跳转
-              </el-button>
+              <span @click="Jump(scope.row.date)" class="button">{{scope.row.date}}</span>
             </template>
           </el-table-column>
         </el-table>
@@ -113,110 +111,126 @@
 export default {
   data () {
     return {
-      tableData1: [{
-                date: '2016-05-03',
-                name: '我乱写',
-                province: '上海',
-                city: '普陀区',
-                address: '上海市普陀区金沙江路 1518 弄',
-                zip: 200333
-              }, {
-                date: '2016-05-02',
-                name: '王小虎',
-                province: '上海',
-                city: '普陀区',
-                address: '上海市普陀区金沙江路 1518 弄',
-                zip: 200333
-              }, {
-                date: '2016-05-04',
-                name: '王小虎',
-                province: '上海',
-                city: '普陀区',
-                address: '上海市普陀区金沙江路 1518 弄',
-                zip: 200333
-              }, {
-                date: '2016-05-01',
-                name: '王小虎',
-                province: '上海',
-                city: '普陀区',
-                address: '上海市普陀区金沙江路 1518 弄',
-                zip: 200333
-              }, {
-                date: '2016-05-08',
-                name: '王小虎',
-                province: '上海',
-                city: '普陀区',
-                address: '上海市普陀区金沙江路 1518 弄',
-                zip: 200333
-              }, {
-                date: '2016-05-06',
-                name: '王小虎',
-                province: '上海',
-                city: '普陀区',
-                address: '上海市普陀区金沙江路 1518 弄',
-                zip: 200333
-              }, {
-                date: '2016-05-07',
-                name: '王小虎',
-                province: '上海',
-                city: '普陀区',
-                address: '上海市普陀区金沙江路 1518 弄',
-                zip: 200333
-              }],
-      tableData2: [{
-                date: '2016-05-03',
-                name: '王小虎',
-                province: '上海',
-                city: '普陀区',
-                address: '上海市普陀区金沙江路 1518 弄',
-                zip: 200333
-              }, {
-                date: '2016-05-02',
-                name: '王小虎',
-                province: '上海',
-                city: '普陀区',
-                address: '上海市普陀区金沙江路 1518 弄',
-                zip: 200333
-              }, {
-                date: '2016-05-04',
-                name: '王小虎',
-                province: '上海',
-                city: '普陀区',
-                address: '上海市普陀区金沙江路 1518 弄',
-                zip: 200333
-              }, {
-                date: '2016-05-01',
-                name: '王小虎',
-                province: '上海',
-                city: '普陀区',
-                address: '上海市普陀区金沙江路 1518 弄',
-                zip: 200333
-              }, {
-                date: '2016-05-08',
-                name: '王小虎',
-                province: '上海',
-                city: '普陀区',
-                address: '上海市普陀区金沙江路 1518 弄',
-                zip: 200333
-              }, {
-                date: '2016-05-06',
-                name: '王小虎',
-                province: '上海',
-                city: '普陀区',
-                address: '上海市普陀区金沙江路 1518 弄',
-                zip: 200333
-              }, {
-                date: '2016-05-07',
-                name: '王小虎',
-                province: '上海',
-                city: '普陀区',
-                address: '上海市普陀区金沙江路 1518 弄',
-                zip: 200333
-              }]
+      tableData1: [
+        {
+          date: '2016-05-03',
+          name: '我乱写',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        },
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+         },
+         {
+          date: '2016-05-08',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+          },
+          {
+            date: '2016-05-06',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          },
+          {
+            date: '2016-05-07',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          }],
+      tableData2: [
+          {
+            date: '2016-05-03',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          },
+          {
+            date: '2016-05-02',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          },
+          {
+            date: '2016-05-04',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          },
+          {
+            date: '2016-05-01',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          },
+          {
+            date: '2016-05-08',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          },
+          {
+            date: '2016-05-06',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+           },
+           {
+            date: '2016-05-07',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          }]
     }
   },
   methods: {
-
+    Jump(data){
+      console.log(data)
+    }
   },
   mounted () {
   },
@@ -235,9 +249,9 @@ export default {
       font-size: 20px;
       letter-spacing: 1px;
       font-weight: 600;
-      padding: 10px;
+      padding: 12px;
       margin: 10px;
-      background:#aaaaff;
+      background:#ffaa92;
       border-radius: 10px;
       color: #ffffff;
     }
@@ -249,11 +263,19 @@ export default {
       font-size: 20px;
       letter-spacing: 1px;
       font-weight: 600;
-      padding: 10px;
+      padding: 12px;
       margin: 10px;
-      background:#ffaa7f;
+      background:#aaaaff;
       border-radius: 10px;
       color: #ffffff;
+    }
+    .button{
+      font-size: 16px;
+      padding: 10px;
+      margin:0;
+    }
+    .button:hover{
+      cursor: pointer;
     }
   }
 }
