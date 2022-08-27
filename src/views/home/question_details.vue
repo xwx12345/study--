51aa3ccc1f961e080c3f25ad07cdc3c24102ac62
IoNaNO <orange0714@foxmail.com>
2022-08-27 20:31:38 +0800

    Add Delete

--- src/views/home/question_details.vue
+++ src/views/home/question_details.vue
@@ -1,24 +1,31 @@
 <template>
   <el-container class="container">
     <div class="header">
-      <div>
-        <el-button icon="el-icon-star-off" circle @click="collectquestion(id)"></el-button>
+      <div class="button">
+        <p>
+          <el-button
+            icon="el-icon-star-off"
+            circle
+            @click="collectquestion(id)"
+          ></el-button>
+        </p>
+        <p>
+          <el-button v-if="usertype === 3" icon="el-icon-delete"></el-button>
+        </p>
       </div>
       <div class="stem">
         <i class="el-icon-s-promotion"></i>
-        {{stem}}
+        {{ stem }}
       </div>
     </div>
     <div class="bottom">
       <div class="up">
         <span>答案</span>
       </div>
-      <el-main >
-        <span class="answer" v-if="answer.length==0">
-        暂无回答
-        </span>
+      <el-main>
+        <span class="answer" v-if="answer.length == 0"> 暂无回答 </span>
         <span v-else v-for="item in answer">
-          <span>{{item}}</span>
+          <span>{{ item }}</span>
           <el-divider></el-divider>
         </span>
       </el-main>
@@ -27,106 +34,116 @@
 </template>
 
 <script>
-import { getQuestion,CollectQuestion,getAnswer} from '@/api/subject';
+import { getQuestion, CollectQuestion, getAnswer } from "@/api/subject";
 
 export default {
-  data () {
+  data() {
     return {
-      id:'',
-      stem:'',
-      answerList:[],
-      post_time:'',
-      img_url:'',
-      answer:[],
-    }
+      id: "",
+      stem: "",
+      answerList: [],
+      post_time: "",
+      img_url: "",
+      answer: [],
+    };
   },
   methods: {
-    collectquestion(data){
-      CollectQuestion(this.$store.getters.user.user_id,data).then((r)=>{
-        if(r.code===0){
-          this.$message(r.message)
-        }
-        else{
-          this.$message.error(r.message)
-        }
-      }).catch((err)=>{
-        console.log(err)
-      })
+    collectquestion(data) {
+      CollectQuestion(this.$store.getters.user.user_id, data)
+        .then((r) => {
+          if (r.code === 0) {
+            this.$message(r.message);
+          } else {
+            this.$message.error(r.message);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
-
   },
-  mounted () {
-  },
-  created () {
-    this.id=this.$route.query.qid;
-    getQuestion(this.id).then((r)=>{
-      if(r.header.code===0){
-        this.stem=r.data.question_stem;
-        this.answerList=r.data.answer_id_list;
-        this.post_time=r.data.post_time;
-        this.img_url=r.data.pic_url;
-        for(var i=0;i<this.answerList.length;i++){
-          getAnswer(this.answerList[i]).then((r)=>{
-            if(r.header.code===0){
-              this.answer.push(r.data.answer_content);
-              }
-              else{
-                this.$message.error(r.header.message)
-              }
-          }).catch((err)=>{
-            console.log(err)
-          })
+  mounted() {},
+  created() {
+    if (this.$store.getters.user) {
+      this.usertype = this.$store.getters.user.user_type;
+    } else {
+      this.usertype = 0;
+    }
+    this.id = this.$route.query.qid;
+    getQuestion(this.id)
+      .then((r) => {
+        if (r.header.code === 0) {
+          this.stem = r.data.question_stem;
+          this.answerList = r.data.answer_id_list;
+          this.post_time = r.data.post_time;
+          this.img_url = r.data.pic_url;
+          for (var i = 0; i < this.answerList.length; i++) {
+            getAnswer(this.answerList[i])
+              .then((r) => {
+                if (r.header.code === 0) {
+                  this.answer.push(r.data.answer_content);
+                } else {
+                  this.$message.error(r.header.message);
+                }
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          }
+        } else {
+          this.$message.error(r.header.message);
         }
-      }
-      else{
-        this.$message.error(r.header.message);
-      }
-    }).catch((err)=>{
-        console.log(err)
       })
-
-  }
-}
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
 </script>
 
 <style scoped lang="scss">
-.container{
+.container {
   width: 100%;
   display: flex;
   flex-direction: column;
   // height: calc(100vh - 80px);
-  .header{
-    background-color: rgba(255,255,255,0.3);
+  .header {
+    background-color: rgba(255, 255, 255, 0.3);
     display: flex;
     justify-content: center;
     margin: 10px;
     border-radius: 10px;
-    .stem{
+    .button {
+      flex-direction: column;
+      display: inline-block;
+      justify-content: space-between;
+    }
+    .stem {
       width: 90%;
       padding: 10px;
       margin: 30px;
       font-size: 18px;
     }
   }
-  .bottom{
-    background-color: rgba(255,255,255,0.3);
+  .bottom {
+    background-color: rgba(255, 255, 255, 0.3);
     margin: 10px;
     border-radius: 10px;
-    .up{
+    .up {
       width: 80%;
       margin: 0 auto;
-      span{
+      span {
         display: inline-block;
         padding: 10px;
         margin: 10px;
-        background:linear-gradient(90deg,#aaaaff,#a2c3ff) ;
+        background: linear-gradient(90deg, #aaaaff, #a2c3ff);
         border-radius: 10px;
         font-weight: 600;
         color: #fff;
         letter-spacing: 4px;
       }
     }
-    .el-main{
+    .el-main {
       width: 80%;
       margin: 0 auto;
       background: rgba(255, 255, 255, 0.8);
